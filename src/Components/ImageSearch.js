7b31f6e7bf88de9696dee2f2c2cc9cf67fa4dd0d
naveen-kk4 <naveennig2001@gmail.com>
2023-12-20 16:04:12 +0530

    bugs fixed

--- src/Components/ImageSearch.js
+++ src/Components/ImageSearch.js
@@ -3,32 +3,50 @@ import axios from "axios";
 
 
 const ImageSearch = ({setText})=>{
-    const[searchInput,setSearchInput] = useState("");
-    useEffect(()=>{
-      searchForImage(null,"random");
-    },[])
+   
+    const[searchInput,setSearchInput] = useState("random");
+    const[page,setPage] = useState(1);
+    
+    
+    useEffect((e)=>{
+      searchForImage(e,false);
+    },[page])
 
     async function searchForImage(e,initialSearch){
         if(e)e.preventDefault();
+        if(initialSearch){
+            setPage(1);
+            return;
+        }
         const result = await axios.get("https://api.unsplash.com/search/photos",{
             headers:{
                 "Accept-Version": "v1"
             },
             params:{
-                query: searchInput || initialSearch,
-                client_id:`${process.env.REACT_APP_UNSPLASH_API_KEY}`
+                query: searchInput,
+                client_id:`${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+               per_page:20,
+                page:page
             }
         });
        
         setText(result.data.results);
+        
 
     }
     return(
         <div className="search-img">
-            <form onSubmit={searchForImage}>
+            <form onSubmit={(e) => {
+      searchForImage(e, true);
+   }}>
                 <input type = "text" placeholder="Search anything..." onChange={(e)=>{setSearchInput(e.target.value);}}></input>
                 <button type="Submit">Search</button>
             </form>
+            {
+                page>1 &&  <button type="button" onClick={()=>{setPage(page-1);}}>Prev</button>
+            }
+            <button type="button" onClick={()=>{setPage(page+1);}}>Next</button>
+           
         </div>
     )
 }
